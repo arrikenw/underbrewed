f26efeda9e126b4ee223885f92a463634dfd5498
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-10-19 18:26:45 +1100

    Implement Item Class and Station Class

---
+++ Assets/Item.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : Interactable
+{
+    // Start is called before the first frame update
+
+    private bool held = false;
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPickup() {
+        held = true;
+        base.OnLeave();
+    }
+
+    public void OnDrop() {
+        held = false;
+    }
+
+    public bool IsHeld() {
+        return held;
+    }
+}
