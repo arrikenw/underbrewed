7fbefa953441a849497819491603897c61a8b669
jfkenna-unimelb <jfkenna@student.unimelb.edu.au>
2020-10-19 18:12:50 +1100

    basic interactable
    + Runs script when player interacts and is holding object

--- Assets/Scenes/arriken/PickUpScript.cs
+++ Assets/Scenes/arriken/PickUpScript.cs
@@ -7,6 +7,9 @@ public class PickUpScript : MonoBehaviour
     private GameObject PickedUp = null;
     private bool IsPickedUp = false;
 
+    private GameObject interactableTarget = null;
+    private Interactable interactable = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,29 @@ public class PickUpScript : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        //interact
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            print("E");
+            if (interactableTarget != null)
+            {
+                if (IsPickedUp == false)
+                {
+                    print("tried to interact but had empty hands");
+                }
+                else
+                {
+                    print("trying to interact");
+                    print(interactable);
+                    interactable.interact(PickedUp);
+                    //remove from hands
+                    PickedUp = null;
+                    IsPickedUp = false;
+                }
+            }
+        }
+
+        //pickup
         if (Input.GetKeyDown(KeyCode.M))
         {
             IsPickedUp = true;
@@ -41,6 +67,11 @@ public class PickUpScript : MonoBehaviour
         {
             PickedUp = other.gameObject;
         }
+
+        if (other.gameObject.name == "Bench"){
+            interactableTarget = other.gameObject;
+            interactable = interactableTarget.GetComponent(typeof(Interactable)) as Interactable;
+        }
     }
 
     private void OnTriggerExit(Collider other) 
@@ -50,10 +81,10 @@ public class PickUpScript : MonoBehaviour
         {
             PickedUp = null;
         }
+
+        //idk if == works here
+        if (interactableTarget == other.gameObject){
+            interactableTarget = null;
+        }
     }
-
-
-
-
-
 }
