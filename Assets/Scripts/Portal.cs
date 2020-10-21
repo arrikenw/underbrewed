c4ca558fb69cb31777983ee3700536bcaa60a5db
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-10-21 18:53:10 +1100

    Implement template code for Delivery Portal

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : Station
+{
+    // TODO: Connect with game/score manager
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    void Update()
+    {
+        Deliver();
+    }
+
+    private void Deliver() {
+        if (base.storedItem != null) {
+            Destroy(base.storedItem);
+        }
+    }
+}
