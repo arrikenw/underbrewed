bfd8a43dd337d75e4736ff2c928e5c29f979027c
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-10-21 17:28:16 +1100

    Implement proper Cauldron

--- Assets/Scripts/Cauldron.cs
+++ Assets/Scripts/Cauldron.cs
@@ -4,88 +4,69 @@ using UnityEngine;
 
 public class Cauldron : Station
 {
-    // // object being processed
-    // [SerializeField] private GameObject currentStoredObject;
+    private GameObject cauldronLiquid;
+    private GameObject bubbles;
+    private GameObject bubbleBurst;
+    [SerializeField] private Color mixColour; // Recording current colour is needed for future implementations
+    private Color baseColour;
 
-    // shading
-    Renderer r;
-    [SerializeField] private Material idleMat;
-    [SerializeField] private Material cookMat;
-
-    // logic
-    // TODO create a lookup table for the costs of each bench type with each item type
-    [SerializeField] private int cookTime = 3; 
-    private int timeUntilComplete = 0;
-    private bool isCooking = false;
-
-    // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
-        r = GetComponent<Renderer>();
-        r.material = idleMat;
+        cauldronLiquid = transform.GetChild(0).gameObject;
+        bubbles = cauldronLiquid.transform.GetChild(0).gameObject;
+        bubbleBurst = bubbles.transform.GetChild(0).gameObject;
+
+
+        baseColour = cauldronLiquid.GetComponent<Renderer>().material.color;
+        mixColour = baseColour;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        Cook();
+        Mix();
     }
 
-    void Cook() {
+    private void Mix() {
+        if (base.storedItem != null && base.storedItem.GetComponent<Ingredient>() != null) {
+            // Retrieve new cauldron liquid colour from stored Item 
+            mixColour = base.storedItem.GetComponent<Ingredient>().GetColor();
 
-        if (!isCooking) {
-            if (base.storedItem != null) {
-                base.canPickup = false;
-                base.locked = true;
-
-                // TODO: remove collision
-
-                // change mat / shader
-                r.material = cookMat;
-
-                // set cooking time etc.
-                // timeUntilComplete = cookTime; 
-                // Note for Joel: logic doesn't have to be done this way, I just wanted to try this hack
-                Destroy(base.storedItem, cookTime);
-                isCooking = true; 
-            }
-        } else {
+            // Destroy stored Item 
+            Destroy(base.storedItem);
             
-            if (base.storedItem == null)
-            {
-                r.material = idleMat;
-
-                // TODO: output processed item
-                isCooking = false;
-            }
-
-            // timeUntilComplete -= 1;
-            // if (timeUntilComplete <= 0)
-            // {
-            //     r.material = idleMat;
-
-            //     // TODO: output processed item and destroy held item
-            //     isCooking = false;
-            // }
+            // Update liquid and bubble colours
+            UpdateColours();
         }
     }
 
-    // public void Interact(GameObject obj)
-    // {
-    //     // currentStoredObject = obj;
-        
-    //     //place above the object
-    //     Vector3 newObjCoords = currentStoredObject.transform.position + (new Vector3(0, 0.5f, 0));
-    //     currentStoredObject.transform.position = newObjCoords;
+    private void UpdateColours() {
+        // Set new cauldron liquid colour
+        cauldronLiquid.GetComponent<Renderer>().material.SetColor("_Color", mixColour);
 
-    //     // TODO: remove collision
+        // Set new bubbles colour
+        // https://docs.unity3d.com/ScriptReference/ParticleSystem.html
+        // https://docs.unity3d.com/ScriptReference/ParticleSystem-main.html
+        // Read above to understand why it must be written this way
+        var bubblesMain = bubbles.GetComponent<ParticleSystem>().main;
+        bubblesMain.startColor = mixColour;
 
-    //     // change mat / shader
-    //     r.material = cookMat;
+        // Set new bubble burst colour
+        var bubbleBurstMain = bubbleBurst.GetComponent<ParticleSystem>().main;
+        bubbleBurstMain.startColor = mixColour;
+    }
 
-    //     // set cooking time etc.
-    //     timeUntilComplete = cookTime;
-    //     isCooking = true; 
-    // }
+    public override void Interact(GameObject other) {
+        print("Interacting with cauldron");
+
+        // Case: Potion
+        if (!mixColour.Equals(baseColour) && other.GetComponent<Potion>() != null) {
+            other.GetComponent<Potion>().SetPotionColor(mixColour);
+            mixColour = baseColour;
+            UpdateColours();
+        }
+    }
+
+
 }
