3294411961208b6621013e006ccd04f97f954916
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-10-21 17:30:31 +1100

    Implement Interaction Method

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,19 +1,15 @@
-using System.Collections;
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour {
     
-    private Material highlighter;
-    private Material original;
     protected Material highlighter;
     protected Material actual;
     protected bool locked = false;
 
     protected virtual void Start() {
         highlighter = Resources.Load<Material>("Highlighter");
-        original = GetComponent<Renderer>().material; 
         actual = GetComponent<Renderer>().material; 
     }
 
@@ -25,11 +21,9 @@ public class Interactable : MonoBehaviour {
 
     public void OnLeave() {
         if (!locked) {
-            GetComponent<Renderer>().material = original;
             GetComponent<Renderer>().material = actual;
         }
     }
 
-    public virtual void Interact() {}
     public virtual void Interact(GameObject other) {}
 } 
