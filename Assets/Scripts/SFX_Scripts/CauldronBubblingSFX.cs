648d167bbe42cc5dc07df88c9be2e00a174fd700
arrikenw <arrikenw@student.unimelb.edu.au>
2020-10-31 19:25:34 +1100

    added new ingredients

---
+++ Assets/Scripts/SFX_Scripts/CauldronBubblingSFX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CauldronBubblingSFX : MonoBehaviour
+{
+    public AudioSource BubblingSFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BubblingSFX.loop = true;
+        BubblingSFX.PlayDelayed(Random.Range(0.0f, 3.0f));
+    }
+}
---
+++ Assets/Scripts/SFX_Scripts/CauldronFireSFX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CauldronFireSFX : MonoBehaviour
+{
+    public AudioSource fireSFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireSFX.loop = true;
+        fireSFX.PlayDelayed(Random.Range(0.0f, 3.0f));
+    }
+
+}
