2575fad93c6d6250069377eb29f77e8e97e70f9c
jfkenna-unimelb <jfkenna@student.unimelb.edu.au>
2020-11-02 17:33:17 +1100

    +Updated tutorial messages : )
    +Updated tutorial messages : )
    +life is pain :flushed:

---
+++ Assets/Scripts/TUTORIALMANAGERSCRIPT.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TUTORIALMANAGERSCRIPT : MonoBehaviour
+{
+    public GameObject UIObject;
+    public GameObject MRCAULDRON;
+
+    void Update()
+    {
+        if (UIObject.transform.childCount == 0)
+        {
+            UIObject.SetActive(false);
+            MRCAULDRON.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (UIObject.activeSelf)
+            {
+                if (UIObject.transform.childCount >= 2)
+                {
+                    UIObject.transform.GetChild(1).gameObject.SetActive(true);
+                }
+                Destroy(UIObject.transform.GetChild(0).gameObject);
+            }
+        }
+    }
+}
